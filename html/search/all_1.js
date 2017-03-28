5c410bfcade55b8f9868a4193ed89fac2ad53459
christ2go <christian@hagemeier.ch>
2017-03-28 18:38:17 +0200

    Clean gh-pages branch

--- html/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['github_2dpages_2dtest',['github-pages-test',['../md__r_e_a_d_m_e.html',1,'']]]
-];
--- html/search/classes_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['javadoc_5ftest',['Javadoc_Test',['../class_javadoc___test.html',1,'']]]
-];
